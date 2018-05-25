3dc39a4043531605fd9b4d259b318e931df74dd8
Antonio Lázaro <antoniolazaro1795@hotmail.com>
2018-05-25 00:22:01 -0600

    cambios

---
+++ ProyecFinal/Clientes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyecFinal
+{
+
+    class Clientes
+    {
+        string nit;
+        string nombre;
+        string apellido;
+        string direccion;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Nit { get => nit; set => nit = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+    }
+}
