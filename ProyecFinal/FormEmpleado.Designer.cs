f4eed83ab464a830c828432030701fae27601da7
Antonio Lázaro <antoniolazaro1795@hotmail.com>
2018-06-09 15:23:16 -0600

    Guarda los datos de la venta

--- ProyecFinal/FormEmpleado.Designer.cs
+++ ProyecFinal/FormEmpleado.Designer.cs
@@ -46,13 +46,18 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 410);
+            this.button1.Location = new System.Drawing.Point(351, 442);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
@@ -62,7 +67,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Location = new System.Drawing.Point(35, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 1;
@@ -70,7 +75,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 27);
+            this.textBox1.Location = new System.Drawing.Point(120, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(152, 20);
             this.textBox1.TabIndex = 2;
@@ -78,7 +83,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(120, 149);
+            this.comboBox1.Location = new System.Drawing.Point(148, 182);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
@@ -86,7 +91,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 149);
+            this.label2.Location = new System.Drawing.Point(63, 182);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 4;
@@ -95,7 +100,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(272, 149);
+            this.label3.Location = new System.Drawing.Point(300, 182);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 5;
@@ -103,7 +108,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(348, 149);
+            this.textBox2.Location = new System.Drawing.Point(376, 182);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
@@ -111,14 +116,14 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(64, 190);
+            this.dataGridView1.Location = new System.Drawing.Point(66, 222);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(691, 214);
+            this.dataGridView1.Size = new System.Drawing.Size(642, 214);
             this.dataGridView1.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(161, 477);
+            this.textBox3.Location = new System.Drawing.Point(163, 509);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(122, 20);
             this.textBox3.TabIndex = 8;
@@ -126,7 +131,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 477);
+            this.label4.Location = new System.Drawing.Point(52, 509);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 13);
             this.label4.TabIndex = 9;
@@ -134,7 +139,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(337, 477);
+            this.button2.Location = new System.Drawing.Point(339, 509);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 10;
@@ -144,7 +149,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(334, 526);
+            this.label5.Location = new System.Drawing.Point(336, 558);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(37, 13);
             this.label5.TabIndex = 11;
@@ -152,7 +157,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(393, 523);
+            this.textBox4.Location = new System.Drawing.Point(395, 555);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 12;
@@ -160,9 +165,9 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(316, 27);
+            this.dataGridView2.Location = new System.Drawing.Point(316, 36);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(465, 88);
+            this.dataGridView2.Size = new System.Drawing.Size(434, 88);
             this.dataGridView2.TabIndex = 13;
             // 
             // label6
@@ -176,7 +181,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(120, 92);
+            this.button3.Location = new System.Drawing.Point(120, 101);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 15;
@@ -186,7 +191,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(120, 54);
+            this.button4.Location = new System.Drawing.Point(120, 63);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 16;
@@ -197,17 +202,64 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(206, 59);
+            this.label7.Location = new System.Drawing.Point(206, 68);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(35, 13);
             this.label7.TabIndex = 17;
             this.label7.Text = "label7";
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(563, 179);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Agregar Producto";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(120, 136);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Fecha";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(38, 7);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Cajero";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(81, 8);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 22;
+            // 
             // FormEmpleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 560);
+            this.ClientSize = new System.Drawing.Size(793, 588);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -255,5 +307,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
--- ProyecFinal/FormEmpleado.cs
+++ ProyecFinal/FormEmpleado.cs
@@ -21,6 +21,22 @@ namespace ProyecFinal
 
         private void button4_Click(object sender, EventArgs e)
         {
+            List<Inventario> invent = new List<Inventario>();
+
+
+            string fileName3 = @"C:\Users\Antonio\source\repos\ProyecFinal\ProyecFinal\bin\Debug\Inventario.txt";
+            FileStream stream3 = new FileStream(fileName3, FileMode.Open, FileAccess.Read);
+            StreamReader reader3 = new StreamReader(stream3);
+            while (reader3.Peek() > -1)
+            {
+                Inventario invetemp = new Inventario();
+                invetemp.Producto = reader3.ReadLine();
+                invetemp.Cantidad = Convert.ToInt16(reader3.ReadLine());
+                invetemp.Precio = Convert.ToDecimal(reader3.ReadLine());
+                invent.Add(invetemp);
+            }
+            reader3.Close();
+
             List<Clientes> clie = new List<Clientes>();
             string fileName = @"C:\Users\Antonio\source\repos\ProyecFinal\ProyecFinal\bin\Debug\Clientes.txt";
             FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
@@ -76,7 +92,10 @@ namespace ProyecFinal
                 }
 
             }
-
+            for(int i=0; i<invent.Count; i++)
+            {
+                comboBox1.Items.Add(invent[i].Producto);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -85,6 +104,70 @@ namespace ProyecFinal
             clientenuevo.Show();
         }
 
-     
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<Inventario> invent = new List<Inventario>();
+
+
+            string fileName3 = @"C:\Users\Antonio\source\repos\ProyecFinal\ProyecFinal\bin\Debug\Inventario.txt";
+            FileStream stream3 = new FileStream(fileName3, FileMode.Open, FileAccess.Read);
+            StreamReader reader3 = new StreamReader(stream3);
+            while (reader3.Peek() > -1)
+            {
+                Inventario invetemp = new Inventario();
+                invetemp.Producto = reader3.ReadLine();
+                invetemp.Cantidad = Convert.ToInt16(reader3.ReadLine());
+                invetemp.Precio = Convert.ToDecimal(reader3.ReadLine());
+                invent.Add(invetemp);
+            }
+            reader3.Close();
+
+            List<Ventas> vent = new List<Ventas>();
+            string fileName = @"C:\Users\Antonio\source\repos\ProyecFinal\ProyecFinal\bin\Debug\Ventas.txt";
+            FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            Ventas vetemp = new Ventas();
+            vetemp.Nitcliente = textBox1.Text;
+            vetemp.Fechaventa = Convert.ToDateTime(dateTimePicker1.Text);
+            vetemp.Producto = comboBox1.Text;
+            vetemp.Cantidad = Convert.ToInt16(textBox2.Text);
+            vetemp.Codigoempleado = textBox5.Text;
+            vent.Add(vetemp);
+
+            writer.WriteLine(textBox1.Text);
+            writer.WriteLine(dateTimePicker1.Text);
+            writer.WriteLine(comboBox1.Text);
+            writer.WriteLine(textBox2.Text);
+            writer.WriteLine(textBox5.Text);
+
+            for (int i=0; i<invent.Count; i++)
+            {
+                if(invent[i].Producto == comboBox1.Text)
+                {
+                    invent[i].Cantidad = invent[i].Cantidad - Convert.ToInt16(textBox2.Text);
+                    break;
+                }
+            }
+
+            //escribe de nuevo el inventario con el producto vendido restado del inventario
+            string fileName5 = @"C:\Users\Antonio\source\repos\ProyecFinal\ProyecFinal\bin\Debug\Inventario.txt";
+            FileStream stream5 = new FileStream(fileName5, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer5 = new StreamWriter(stream5);
+
+
+            for (int i = 0; i < invent.Count; i++)
+            {
+                writer5.WriteLine(invent[i].Producto);
+                writer5.WriteLine(invent[i].Cantidad);
+                writer5.WriteLine(invent[i].Precio);
+            }
+
+            writer.Close();
+
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = vent;
+            dataGridView1.Refresh();
+        }
     }
 }
